bfcacc22e6fe2499eda64a5a9619d7dcecfa81c4
Denis <dshibitov@gmail.com>
2022-11-06 21:48:02 +0300

    homework1: github actions

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="ml_project",
     author="Denis Shibitov",
@@ -11,8 +14,6 @@ setuptools.setup(
     keywords="mlops, homework",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/made-mlops-2022/denis_shibitov",
-    # package_dir={"": "ml_project"},
     packages=['ml_project'],
     version="0.1.1",
     classifiers=[
@@ -21,34 +22,5 @@ setuptools.setup(
         "Programming Language :: Python",
     ],
     python_requires=">=3.7",
-    install_requires=[
-        "matplotlib",
-        "pandas",
-        "numpy",
-        "scikit-learn",
-        "pandas",
-        # "dvc[s3]",
-        "marshmallow_dataclass",
-        # "mlflow",
-    ],
-    extras_require={
-        # "dev": [
-        #     "wemake-python-styleguide",
-        #     "mypy",
-        #     "black",
-        #     "jupyterlab",
-        #     "seaborn",
-        #     "types-PyYAML",
-        # ],
-        # "tests": [
-        #     "sdv",
-        #     "pytest",
-        #     "pytest-dotenv",
-        # ],
-    },
-    # entry_points={
-    #     'console_scripts': [
-    #         'classification = ml_project.__main__:main',
-    #     ],
-    # },
+    install_requires=required
 )
